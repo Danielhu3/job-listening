6db25d2331a36ee8b45fb82e7aa68912b15155e8
Danielhu3 <danielhu3123@gmail.com>
2022-11-22 16:48:02 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import {createGlobalStyle} from 'styled-components'
+
+export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@500;700&display=swap');
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'League Spartan', sans-serif;
+        box-sizing: border-box;
+    }
+
+    html{
+        font-size: 62.5%;
+    }
+
+    body{
+        font-size: 1.5rem;
+        font-weight: 500;
+    }
+
+    .App{
+        height: 100vh;
+        display: grid;
+        grid-template-rows: 2fr 8fr;
+        grid-template-areas: 'header'
+                             'main';
+    }
+
+
+`
